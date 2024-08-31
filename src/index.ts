372423cb8ff15000144ec48d45ffc78f87dbe620
TristHund <TristanHundley@protonmail.com>
2024-08-31 01:04:50 -0600

    conversion to hono progress, issue with build currently

--- src/index.ts
+++ src/index.ts
@@ -2,73 +2,225 @@ import {
   ActionGetResponse,
   ActionPostRequest,
   ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
 } from "@solana/actions";
 import {
+  clusterApiUrl,
   Connection,
   PublicKey,
-  SystemProgram,
-  TransactionMessage,
-  VersionedTransaction,
+  Transaction,
 } from "@solana/web3.js";
+import * as multisig from "@sqds/multisig";
+import * as anchor from "@coral-xyz/anchor";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { Buffer } from "node:buffer";
-
-if (globalThis.Buffer === undefined) {
-  globalThis.Buffer = Buffer;
-}
-
-// you should use a private RPC here
-const connection = new Connection("https://api.mainnet-beta.solana.com");
 
 const app = new Hono();
 
-// see https://solana.com/docs/advanced/actions#options-response
 app.use(
   cors({
     origin: "*",
     allowHeaders: ["Content-Type", "Authorization", "Accept-Encoding"],
-    allowMethods: ["GET", "POST", "PUT", "OPTIONS"],
-  })
+    allowMethods: ["GET", "POST", "OPTIONS"],
+  }),
 );
 
-app.get("/", (c) => {
-  const response: ActionGetResponse = {
-    title: "Sign the following Transaction",
-    description: "This will contain transaction details for the user to review before signing",
-    icon: "https://img.fotofolio.xyz/?url=https%3A%2F%2Fraw.githubusercontent.com%2Fsolana-labs%2Ftoken-list%2Fmain%2Fassets%2Fmainnet%2FSo11111111111111111111111111111111111111112%2Flogo.png",
-    links: {
-      actions: [
-        {
-          "label": "Approve",
-          "href": "/api/approve",
-        },
-        {
-          "label": "Reject",
-          "href": "/api/reject",
-        },
-      ]
-    }
-  };
+async function validateQueryParams(requestUrl) {
+  const connection = new Connection(clusterApiUrl("mainnet-beta"));
+  let squad = new PublicKey("Gr5FaqkMmypxUJfADQsoYN3moknprc5LzMF2qh3SiP8m");
+  let action = requestUrl.searchParams.get("action");
+  let transactionIndex = 1;
 
-  return c.json(response);
+  try {
+    if (requestUrl.searchParams.get("squad")) {
+      squad = new PublicKey(requestUrl.searchParams.get("squad"));
+    }
+  } catch (err) {
+    throw err;
+  }
+
+  const multisigInfo = await multisig.accounts.Multisig.fromAccountAddress(
+    connection,
+    squad,
+  );
+
+  transactionIndex = Number(multisigInfo.transactionIndex);
+
+  try {
+    if (requestUrl.searchParams.get("tx")) {
+      transactionIndex = Number(requestUrl.searchParams.get("tx"));
+    }
+  } catch (err) {
+    throw err;
+  }
+
+  return {
+    squad,
+    transactionIndex,
+    action,
+  };
+}
+
+app.get("/", async (c) => {
+  try {
+    const requestUrl = new URL(c.req.url);
+    const { squad, transactionIndex } = await validateQueryParams(requestUrl);
+
+    const baseHref = new URL(
+      `/api/action/approve?squad=${squad}&tx=${transactionIndex}`,
+      requestUrl.origin,
+    ).toString();
+
+    const vault = multisig.getVaultPda({
+      multisigPda: new PublicKey(squad),
+      index: 0,
+    });
+
+    const multisigInfo = await fetch(
+      `https://v4-api.squads.so/multisig/${vault[0].toString()}`,
+    ).then((res) => res.json());
+
+    const meta = multisigInfo.metadata;
+
+    const payload = {
+      title: `Approve ${meta.name} Transaction`,
+      icon: "https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/",
+      description: `Cast your vote on transaction #${transactionIndex} for ${meta.name}`,
+      label: "SquadsTransaction",
+      links: {
+        actions: [
+          { label: "Approve", href: `${baseHref}&action=Approve` },
+          { label: "Reject", href: `${baseHref}&action=Reject` },
+          {
+            label: "Approve & Execute",
+            href: `${baseHref}&action=ApproveExecute`,
+          },
+        ],
+      },
+    };
+
+    return c.json(payload, 200, ACTIONS_CORS_HEADERS);
+  } catch (err) {
+    console.log(err);
+    return c.json({ error: "An error occurred" }, 500, ACTIONS_CORS_HEADERS);
+  }
 });
 
 app.post("/", async (c) => {
-  const req = await c.req.json<ActionPostRequest>();
-  //Review and refine transaction logic here
-  const transaction = await validateTransaction(new PublicKey(req.account));
+  try {
+    const requestUrl = new URL(c.req.url);
+    let { squad, transactionIndex, action } =
+      await validateQueryParams(requestUrl);
 
-  const response: ActionPostResponse = {
-    transaction: Buffer.from(transaction.serialize()).toString("base64"),
-  };
+    const body = await c.req.json();
 
-  return c.json(response);
+    let account;
+    try {
+      account = new PublicKey(body.account);
+    } catch (err) {
+      return c.text('Invalid "account" provided', 400, ACTIONS_CORS_HEADERS);
+    }
+
+    const connection = new Connection(clusterApiUrl("mainnet-beta"));
+    const vault = multisig.getVaultPda({
+      multisigPda: new PublicKey(squad),
+      index: 0,
+    });
+
+    const multisigInfo = await fetch(
+      `https://v4-api.squads.so/multisig/${vault[0].toString()}`,
+    ).then((res) => res.json());
+
+    const meta = multisigInfo.metadata;
+
+    const transaction = new Transaction();
+    transaction.feePayer = account;
+
+    if (action === "Approve") {
+      transaction.add(
+        await multisig.instructions.proposalApprove({
+          multisigPda: squad,
+          transactionIndex: BigInt(transactionIndex),
+          member: account,
+          programId: multisig.PROGRAM_ID,
+        }),
+      );
+    } else if (action === "Reject") {
+      transaction.add(
+        await multisig.instructions.proposalReject({
+          multisigPda: squad,
+          transactionIndex: BigInt(transactionIndex),
+          member: account,
+          programId: multisig.PROGRAM_ID,
+        }),
+      );
+    } else if (action === "ApproveExecute") {
+      transaction.add(
+        await multisig.instructions.proposalApprove({
+          multisigPda: squad,
+          transactionIndex: BigInt(transactionIndex),
+          member: account,
+          programId: multisig.PROGRAM_ID,
+        }),
+        (
+          await multisig.instructions.vaultTransactionExecute({
+            connection,
+            multisigPda: squad,
+            transactionIndex: BigInt(transactionIndex),
+            member: account,
+            programId: multisig.PROGRAM_ID,
+          })
+        ).instruction,
+      );
+    } else if (action === "Simulate") {
+      const [transaction] = await PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("multisig"),
+          new PublicKey(squad).toBuffer(),
+          Buffer.from("transaction"),
+          new anchor.BN(transactionIndex).toArrayLike(Buffer, "le", 8),
+        ],
+        multisig.PROGRAM_ID,
+      );
+
+      const transactionInfo =
+        await multisig.accounts.VaultTransaction.fromAccountAddress(
+          connection,
+          transaction,
+        );
+      const message = transactionInfo.serialize();
+
+      return c.redirect(`https://explorer.solana.com/tx/inspector/${message}`);
+    } else {
+      return c.text(
+        "No supported action was selected",
+        400,
+        ACTIONS_CORS_HEADERS,
+      );
+    }
+
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    const payload = await createPostResponse({
+      fields: {
+        transaction,
+        message: `${action === "Approve"
+            ? "Approved"
+            : action === "Reject"
+              ? "Rejected"
+              : "Approved and executed"
+          } transaction #${transactionIndex} for ${meta.name}`,
+      },
+    });
+
+    return c.json(payload, 200, ACTIONS_CORS_HEADERS);
+  } catch (err) {
+    console.log(err);
+    return c.json({ error: "An error occurred" }, 500, ACTIONS_CORS_HEADERS);
+  }
 });
-
-async function validateTransaction(payer: PublicKey) {
- //To be built to handle the transaction validation logic on the squad wallet
-  // connect with wallet, then approve or deny the transaction
-  return 0;}
 
 export default app;
